e602b5cce4e69ae0eb0d96ff2c799ac674afd2c8
Karolina Lach <karolina.lach1@edu.uekat.pl>
2024-01-28 13:44:57 +0100

    add main file K

--- hello.py
+++ hello.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import cv2
 
 app = Flask(__name__)
 api = Api(app)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('dworzec.jpg')
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+
+        return {'count': len(boxes)}
 
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-
+api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
